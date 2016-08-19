fcf2e9b38dae59aea699811590c7170522ff7219
okazbb <okazbb@gmail.com>
2016-08-19 12:58:15 +0900

    change analytics id

--- index.php
+++ index.php
@@ -50,8 +50,18 @@
         }
     </script>
     <script src="js/video_control.js"></script>
-    <script>
-
+    <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-9797989-1']);
+        _gaq.push(['_trackPageview']);
+        (function () {
+            var ga = document.createElement('script');
+            ga.type = 'text/javascript';
+            ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(ga, s);
+        })();
     </script>
 </head>
 <body>
